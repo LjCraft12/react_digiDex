897d07d4af08475813fdc73350ff69a34902b73d
Perry Craft <LjCraft12@gmail.com>
2019-06-28 14:45:15 -0400

    Added ability to login user and login after registration

--- controllers/contacts.controller.js
+++ controllers/contacts.controller.js
@@ -1,15 +1,34 @@
+/*
+*  @route  GET api/contacts
+*  @desc   Get user contacts
+*  @access Private
+* */
 exports.getUserContacts = (req, res) => {
     res.send('getting user contacts')
 };
 
+/*
+*  @route  POST api/contacts
+*  @desc   Add contact to user
+*  @access Private
+* */
 exports.postAddNewContact = (req, res) => {
     res.send('Adding contacts')
 };
-
+/*
+*  @route  PUT api/contacts/:id
+*  @desc   Edit user contact
+*  @access Private
+* */
 exports.updateUserContact = (req, res) => {
     res.send('Updated contact')
 };
 
+/*
+*  @route  DELETE api/contacts/:id
+*  @desc   Delete contact from user
+*  @access Private
+* */
 exports.deleteUserContact = (req, res) => {
     res.send('Contact deleted')
 };
